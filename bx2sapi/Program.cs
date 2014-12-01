0934f22bbd3632bf1fa7a1541c1467292341f78d
VictorNS <VictorNS@narod.ru>
2014-12-01 21:49:54 +0600

    1.1.0.0

--- bx2sapi/Program.cs
+++ bx2sapi/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.AccessControl;
 using System.Text.RegularExpressions;
 using Excel;
 
@@ -56,154 +55,234 @@ Example:
 				var outFile = Path.Combine(Path.GetDirectoryName(inFile), Path.GetFileNameWithoutExtension(inFile));
 				var outFileRuEn = Path.ChangeExtension(outFile + " RuEn", "txt");
 				var outFileEnRu = Path.ChangeExtension(outFile + " EnRu", "txt");
+				var outFileSamples = Path.ChangeExtension(outFile + " Samples", "txt");
 				#endregion verify parameters
-				var data = new List<Raw>();
-				#region read data
-				if (".xlsx" == Path.GetExtension(inFile))
-				{
-					var worksheet = Workbook.Worksheets(inFile).FirstOrDefault();
-					if (worksheet == null)
-					{
-						Console.WriteLine("Workbook is empty.");
-						return;
-					}
-					foreach (var excelRow in worksheet.Rows)
-					{
-						data.Add(new Raw
-						{
-							Eng = excelRow.GetText(0),
-							Rus = excelRow.GetText(2),
-							EngExample = excelRow.GetText(3),
-							RusExample = excelRow.GetText(4)
-						});
-					}
-				}
-				else
-				{
-					using (var file = new StreamReader(inFile, System.Text.Encoding.GetEncoding(1251)))
-					{
-						string line;
-						while ((line = file.ReadLine()) != null)
-							data.Add(new Raw(line));
-					}
-				}
-				#endregion read data
-				#region parse data
-				var rxR = new RegexpHelper();
-				foreach (var raw in data)
-				{
-					raw.IsPhrase = raw.Rus.Contains("(фраза)");
-					raw.Rus = raw.Rus.Replace("(фраза)", "");
-					//
-					var rusprp = rxR.ReplaceAbr(raw.Rus).Replace("(", " (").Replace(")", ") ").Replace("  ", " ").Replace(" ,", ",").Replace(" .", ".").Trim();
-					raw.RusTran = rusprp;
-					raw.RusClear = rxR.RemoveBrackets(rusprp).Replace("  ", " ").Replace(" ,", ",").Replace(" .", ".").Trim();
-					//
-					raw.EngClear = rxR.RemoveBrackets(raw.Eng).Replace("  ", " ").Replace(" ,", ",").Replace(" .", ".").Trim();
-					raw.RusEnd = (raw.RusClear.EndsWith("...")) ? " the phrase" :
-						(raw.RusClear.EndsWith("?")) ? " the question" :
-						(raw.RusClear.EndsWith(".") || raw.Rus.EndsWith("!")) ? " the sentence" : "";
-					raw.EngExampleNorm = rxR.Normilize(raw.EngExample);
-					raw.EngNorm = rxR.Normilize(raw.Eng);
-					raw.RusExampleNorm = rxR.Normilize(raw.RusExample);
-					raw.RusNorm = rxR.Normilize(raw.Rus);
-				}
-				#endregion parse data
-				#region write RuEn file
-				using (var fs = new FileStream(outFileRuEn, FileMode.OpenOrCreate))
-				{
-					using (var writer = new StreamWriter(fs, System.Text.Encoding.GetEncoding(1251)))
-					{
-						foreach (var raw in data)
-						{
-							#region russian
-							if (-1 == raw.RusTran.IndexOf('('))
-								writer.WriteLine(@"listen and translate{1}<silence msec=""400""/><lang langid=""419"">{0}</lang>"
-									, raw.RusTran, raw.RusEnd);
-							else
-								writer.WriteLine(@"listen<silence msec=""400""/><lang langid=""419"">{0}</lang><silence msec=""400""/>translate{2}<lang langid=""419"">{1} {3}</lang>"
-									, raw.RusTran, raw.RusClear, raw.RusEnd, (raw.IsPhrase ? "(фраза)" : ""));
-							#endregion russian
-							#region english
-							if (-1 == raw.Eng.IndexOf('('))
-							{
-								if (-1 == raw.Eng.Trim().IndexOf(' ') && -1 == raw.Eng.Trim().IndexOf('/'))
-									writer.WriteLine(@"<silence msec=""{1}""/>{0}<silence msec=""500""/>{0}", raw.Eng, minPause);
-								else
-									writer.WriteLine(@"<silence msec=""{1}""/>{0}", raw.Eng, minPause);
-							}
-							else
-							{
-								if (-1 == raw.EngClear.Trim().IndexOf(' '))
-									writer.WriteLine(@"<silence msec=""{2}""/>{0}<silence msec=""500""/>{0}<silence msec=""500""/>{1}"
-										, raw.EngClear, raw.Eng, minPause);
-								else
-									writer.WriteLine(@"<silence msec=""{2}""/>{0}<silence msec=""500""/>{1}"
-										, raw.EngClear, raw.Eng, minPause);
-							}
-							#endregion english
-							#region example
-							//if (!String.IsNullOrWhiteSpace(raw.EngExample))
-							//{
-							//	if (raw.EngExampleNorm != raw.EngNorm)
-							//	{
-							//		writer.WriteLine(@"<silence msec=""1900""/>{0}", raw.EngExample);
-							//		if (!String.IsNullOrWhiteSpace(raw.RusExample))
-							//			writer.WriteLine(@"<silence msec=""1500""/><lang langid=""419"">{0}</lang>", raw.RusExample);
-							//	}
-							//}
-							#endregion example
-							writer.WriteLine(@"<silence msec=""{0}""/>", silentPause);
-							writer.WriteLine();
-						}
-						writer.WriteLine(@"The lesson is over.<silence msec=""400""/>Come back and listen to me again!<silence msec=""5000""/>");
-					}
-				}
+
+				var data = ReadDataFromFile(inFile);
+
+				ParseData(data);
+
+				WriteRuEn(outFileRuEn, data, minPause, silentPause);
 				Console.WriteLine(outFileRuEn);
-				#endregion read RuEn file
-				#region write EnRu file
-				using (var fs = new FileStream(outFileEnRu, FileMode.OpenOrCreate))
-				{
-					using (var writer = new StreamWriter(fs, System.Text.Encoding.GetEncoding(1251)))
-					{
-						foreach (var raw in data)
-						{
-							writer.WriteLine(@"listen and translate");
-							if (!String.IsNullOrWhiteSpace(raw.EngExample) && raw.EngExampleNorm != raw.EngNorm)
-								writer.WriteLine(@"<silence msec=""400""/>{0}", raw.EngExample);
 
-							#region english
-							if (-1 == raw.Eng.IndexOf('('))
-								writer.WriteLine(@"<silence msec=""800""/>{0}", raw.Eng);
-							else
-								writer.WriteLine(@"<silence msec=""800""/>{0}<silence msec=""800""/>{1}", raw.Eng, raw.EngClear);
-							#endregion english
-							#region russian
-							if (-1 == raw.RusTran.IndexOf('('))
-								writer.WriteLine(@"<silence msec=""{1}""/><lang langid=""419"">{0}</lang>"
-									, raw.RusTran, minPause);
-							else
-								writer.WriteLine(@"<silence msec=""{2}""/><lang langid=""419"">{0}</lang><silence msec=""800""/><lang langid=""419"">{1}</lang>"
-									, raw.RusClear, raw.RusTran, minPause);
-							#endregion russian
-
-							if (!String.IsNullOrWhiteSpace(raw.EngExample) && !String.IsNullOrWhiteSpace(raw.RusExample)
-								&& (raw.EngExampleNorm != raw.EngNorm || raw.RusExampleNorm != raw.RusNorm))
-								writer.WriteLine(@"<silence msec=""1500""/><lang langid=""419"">{0}</lang>", raw.RusExample);
-
-							writer.WriteLine(@"<silence msec=""{0}""/>", silentPause);
-							writer.WriteLine();
-						}
-						writer.WriteLine(@"The lesson is over.<silence msec=""400""/>Come back and listen to me again!<silence msec=""5000""/>");
-					}
-				}
+				WriteEnRu(outFileEnRu, data, minPause, silentPause);
 				Console.WriteLine(outFileEnRu);
-				#endregion read EnRu file
+
+				WriteSamples(outFileSamples, data, silentPause);
+				Console.WriteLine(outFileSamples);
 			}
 			catch (Exception ex)
 			{
 				Console.WriteLine(ex.ToString());
 				Console.ReadKey();
+			}
+		}
+
+		private static void ParseData(IEnumerable<Raw> data)
+		{
+			var rxR = new RegexpHelper();
+			foreach (var raw in data)
+			{
+				raw.IsPhrase = raw.Rus.Contains("(фраза)");
+				raw.Rus = raw.Rus.Replace("(фраза)", "");
+				//
+				var rusprp =
+					rxR.ReplaceAbr(raw.Rus)
+						.Replace("(", " (")
+						.Replace(")", ") ")
+						.Replace("  ", " ")
+						.Replace(" ,", ",")
+						.Replace(" .", ".")
+						.Trim();
+				raw.RusTran = rusprp;
+				raw.RusClear = rxR.RemoveBrackets(rusprp).Replace("  ", " ").Replace(" ,", ",").Replace(" .", ".").Trim();
+				//
+				raw.EngClear = rxR.RemoveBrackets(raw.Eng).Replace("  ", " ").Replace(" ,", ",").Replace(" .", ".").Trim();
+				raw.RusEnd = (raw.RusClear.EndsWith("..."))
+					? " the phrase"
+					: (raw.RusClear.EndsWith("?"))
+						? " the question"
+						: (raw.RusClear.EndsWith(".") || raw.Rus.EndsWith("!")) ? " the sentence" : "";
+				raw.EngExampleNorm = rxR.Normilize(raw.EngExample);
+				raw.EngNorm = rxR.Normilize(raw.Eng);
+				raw.RusExampleNorm = rxR.Normilize(raw.RusExample);
+				raw.RusNorm = rxR.Normilize(raw.Rus);
+			}
+		}
+
+		private static List<Raw> ReadDataFromFile(string inFile)
+		{
+			var data = new List<Raw>();
+
+			if (".xlsx" == Path.GetExtension(inFile))
+			{
+				var worksheet = Workbook.Worksheets(inFile).FirstOrDefault();
+				if (worksheet == null)
+				{
+					Console.WriteLine("Workbook is empty.");
+					return data;
+				}
+				foreach (var excelRow in worksheet.Rows)
+				{
+					data.Add(new Raw
+					{
+						Eng = excelRow.GetText(0),
+						Rus = excelRow.GetText(2),
+						EngExample = excelRow.GetText(3),
+						RusExample = excelRow.GetText(4)
+					});
+				}
+			}
+			else
+			{
+				using (var file = new StreamReader(inFile, System.Text.Encoding.GetEncoding(1251)))
+				{
+					string line;
+					while ((line = file.ReadLine()) != null)
+						data.Add(new Raw(line));
+				}
+			}
+
+			return data;
+		}
+
+		private static void WriteEnRu(string outFile, IEnumerable<Raw> data, int minPause, int silentPause)
+		{
+			if (File.Exists(outFile))
+				File.Delete(outFile);
+			using (var fs = new FileStream(outFile, FileMode.OpenOrCreate))
+			{
+				using (var writer = new StreamWriter(fs, System.Text.Encoding.GetEncoding(1251)))
+				{
+					foreach (var raw in data)
+					{
+						writer.WriteLine(@"listen and translate");
+						if (!String.IsNullOrWhiteSpace(raw.EngExample) && raw.EngExampleNorm != raw.EngNorm)
+							writer.WriteLine(@"<silence msec=""400""/>{0}", raw.EngExample);
+
+						#region english
+
+						if (-1 == raw.Eng.IndexOf('('))
+							writer.WriteLine(@"<silence msec=""800""/>{0}", raw.Eng);
+						else
+							writer.WriteLine(@"<silence msec=""800""/>{0}<silence msec=""800""/>{1}", raw.Eng, raw.EngClear);
+
+						#endregion english
+
+						#region russian
+
+						if (-1 == raw.RusTran.IndexOf('('))
+							writer.WriteLine(@"<silence msec=""{1}""/><lang langid=""419"">{0}</lang>"
+								, raw.RusTran, minPause);
+						else
+							writer.WriteLine(
+								@"<silence msec=""{2}""/><lang langid=""419"">{0}</lang><silence msec=""800""/><lang langid=""419"">{1}</lang>"
+								, raw.RusClear, raw.RusTran, minPause);
+
+						#endregion russian
+
+						if (!String.IsNullOrWhiteSpace(raw.EngExample) && !String.IsNullOrWhiteSpace(raw.RusExample)
+						    && (raw.EngExampleNorm != raw.EngNorm && raw.RusExampleNorm != raw.RusNorm))
+							writer.WriteLine(@"<silence msec=""1500""/><lang langid=""419"">{0}</lang>", raw.RusExample);
+
+						writer.WriteLine(@"<silence msec=""{0}""/>", silentPause);
+						writer.WriteLine();
+					}
+					writer.WriteLine(
+						@"The lesson is over.<silence msec=""400""/>Come back and listen to me again!<silence msec=""5000""/>");
+				}
+			}
+		}
+
+		private static void WriteSamples(string outFile, IEnumerable<Raw> data, int silentPause)
+		{
+			if (File.Exists(outFile))
+				File.Delete(outFile);
+			var dataSamples = data.Where(it => !String.IsNullOrWhiteSpace(it.EngExample))
+				.Where(it => !String.IsNullOrWhiteSpace(it.RusExample))
+				.GroupBy(it => it.EngExample);
+			using (var fs = new FileStream(outFile, FileMode.OpenOrCreate))
+			{
+				using (var writer = new StreamWriter(fs, System.Text.Encoding.GetEncoding(1251)))
+				{
+					foreach (var dataSample in dataSamples)
+					{
+						var raw = dataSample.FirstOrDefault();
+						writer.WriteLine(@"listen and translate");
+						writer.WriteLine(@"<silence msec=""400""/>{0}", raw.EngExample);
+						writer.WriteLine(@"<silence msec=""1500""/><lang langid=""419"">{0}</lang>", raw.RusExample);
+						writer.WriteLine(@"<silence msec=""{0}""/>", silentPause);
+						writer.WriteLine();
+					}
+					writer.WriteLine(
+						@"The lesson is over.<silence msec=""400""/>Come back and listen to me again!<silence msec=""5000""/>");
+				}
+			}
+		}
+
+		private static void WriteRuEn(string outFile, IEnumerable<Raw> data, int minPause, int silentPause)
+		{
+			if (File.Exists(outFile))
+				File.Delete(outFile);
+			using (var fs = new FileStream(outFile, FileMode.OpenOrCreate))
+			{
+				using (var writer = new StreamWriter(fs, System.Text.Encoding.GetEncoding(1251)))
+				{
+					foreach (var raw in data)
+					{
+						#region russian
+
+						if (-1 == raw.RusTran.IndexOf('('))
+							writer.WriteLine(@"listen and translate{1}<silence msec=""400""/><lang langid=""419"">{0}</lang>"
+								, raw.RusTran, raw.RusEnd);
+						else
+							writer.WriteLine(
+								@"listen<silence msec=""400""/><lang langid=""419"">{0}</lang><silence msec=""400""/>translate{2}<lang langid=""419"">{1} {3}</lang>"
+								, raw.RusTran, raw.RusClear, raw.RusEnd, (raw.IsPhrase ? "(фраза)" : ""));
+
+						#endregion russian
+
+						#region english
+
+						if (-1 == raw.Eng.IndexOf('('))
+						{
+							if (-1 == raw.Eng.Trim().IndexOf(' ') && -1 == raw.Eng.Trim().IndexOf('/'))
+								writer.WriteLine(@"<silence msec=""{1}""/>{0}<silence msec=""500""/>{0}", raw.Eng, minPause);
+							else
+								writer.WriteLine(@"<silence msec=""{1}""/>{0}", raw.Eng, minPause);
+						}
+						else
+						{
+							if (-1 == raw.EngClear.Trim().IndexOf(' '))
+								writer.WriteLine(@"<silence msec=""{2}""/>{0}<silence msec=""500""/>{0}<silence msec=""500""/>{1}"
+									, raw.EngClear, raw.Eng, minPause);
+							else
+								writer.WriteLine(@"<silence msec=""{2}""/>{0}<silence msec=""500""/>{1}"
+									, raw.EngClear, raw.Eng, minPause);
+						}
+
+						#endregion english
+
+						#region example
+
+						//if (!String.IsNullOrWhiteSpace(raw.EngExample))
+						//{
+						//	if (raw.EngExampleNorm != raw.EngNorm)
+						//	{
+						//		writer.WriteLine(@"<silence msec=""1900""/>{0}", raw.EngExample);
+						//		if (!String.IsNullOrWhiteSpace(raw.RusExample))
+						//			writer.WriteLine(@"<silence msec=""1500""/><lang langid=""419"">{0}</lang>", raw.RusExample);
+						//	}
+						//}
+
+						#endregion example
+
+						writer.WriteLine(@"<silence msec=""{0}""/>", silentPause);
+						writer.WriteLine();
+					}
+					writer.WriteLine(
+						@"The lesson is over.<silence msec=""400""/>Come back and listen to me again!<silence msec=""5000""/>");
+				}
 			}
 		}
 	}
